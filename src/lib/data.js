6b0919e69fd446c543528626ec021bc411dd02f0
Ismael-Rvas <ismael.rivas.developer@gmail.com>
2025-02-03 20:49:09 +0100

    ejercio2

---
+++ src/lib/data.js
@@ -0,0 +1,65 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+
+// ---------------------   PACIENTES  -----------------------
+
+async function obtenerPacientes() {
+    const pacientes = await prisma.paciente.findMany()
+    return pacientes
+}
+
+
+async function obtenerPacientes(id) {
+    const pacientes = await prisma.paciente.findUnique({
+        where: { id: +id },
+        include: {
+            planta: true,
+            medicina: true,
+        }
+    })
+    return pacientes
+}
+
+
+// ---------------------   MEDICINAS  -----------------------
+
+async function obtenerMedicinas() {
+    const medicinas = await prisma.medicina.findMany()
+    return medicinas
+}
+
+
+async function obtenerMedicinas(id) {
+    const medicina = await prisma.medicina.findUnique({
+        where: { id: +id },
+        
+    })
+    return medicina
+}
+
+// ---------------------   PLANTA -----------------------
+
+async function obtenerPlantas() {
+    const plantas = await prisma.planta.findMany()
+    return plantas
+}
+
+
+async function obtenerPlantas(id) {
+    const planta = await prisma.planta.findUnique({
+        where: { id: +id }
+    })
+    return planta
+}
+
+
+
+export {
+    obtenerPacientes,
+    obtenerPacientes,
+    obtenerMedicinas,
+    obtenerMedicinas,
+    obtenerPlantas,
+    obtenerPlantas
+}
